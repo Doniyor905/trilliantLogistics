07641fdb32714f77914bb1e7704b5b1ac4ae0ca2
Doniyor <usupovd276@gmail.com>
2024-10-02 15:18:15 +0300

    aka

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,88 +1,76 @@
-import React from 'react'
-import "./About.scss"
-import { motion } from 'framer-motion'
-
+import React from 'react';
+import './About.scss';
+import { motion } from 'framer-motion';
 
 const About = () => {
+  const AnimateItemLeft = {
+    hidden: {
+      x: -100,
+      opacity: 0,
+    },
+    visible: (custom) => ({
+      x: 0,
+      opacity: 1,
+      transition: { delay: custom * 0.5 },
+    }),
+  };
 
-    const AnimateItemLeft = {
-        hidden: {
-            x: -100,
-            opacity: 0,
-        },
-        visible: custom => ({
-            x: 0,
-            opacity: 1,
-            transition: { delay: custom * 0.5 },
-        })
-    }
-
-    
-    const AnimateItemRight = {
-        hidden: {
-            scale:0,
-            opacity:0,
-        },
-        visible: custom => ({
-            scale:1,
-            opacity:1,
-            transition: {delay:custom * 0.5},
-        })
-    }
-    return (
-        <motion.div
-            viewport={{ once: true }}
-            initial="hidden"
-            whileInView="visible"
-            className='about'
-            id="company">
-            <div className="container">
-                <div className="about__inner">
-                    <motion.div variants={AnimateItemLeft} className="about__left">
-                        <h3 className="about__left-title">
-                            Мы даем гарантию в срок и
-                            держим свое слово.
-                        </h3>
-                        <div className="about__left-text">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting
-                            industry. Lorem Ipsum has been the industry's standard dummy
-                            text ever since the 1500s, when an unknown printer took a galley
-                            of type and scrambled it to make a type specimen book. It has
-                            survived not only five centuries, but also the leap into electronic
-                            typesetting, remaining essentially unchanged. It was popularised
-                            in the 1960s with the release of Letraset sheets containing
-                        </div>
-                        <h3 className="about__left-subtitle">
-                            B2B целевая аудитория:
-                        </h3>
-                        <div className="about__left-subtitle-text">
-                            Производители, кторым нужнапомощь в экспорте их товаров
-                            в страны ближнего востока или в импорте из стран ближнего
-                            востока в узбекистан.
-                        </div>
-                    </motion.div>
-                    <motion.div variants={AnimateItemRight} custom={2} className="about__right">
-                        <form className="about__form">
-                            <h3 className='about__form-title'>Связаться с нами</h3>
-                            <label className='about__form-input'>
-                                <p>Имя</p>
-                                <input type="text" />
-                            </label>
-                            <label className='about__form-input'>
-                                <p>Телефон</p>
-                                <input type="text" />
-                            </label>
-                            <label className='about__form-input'>
-                                <p>Адрес электронной почты</p>
-                                <input type="text" />
-                            </label>
-                            <button className='about__form-button'>Отправить</button>
-                        </form>
-                    </motion.div>
-                </div>
+  const AnimateItemRight = {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    visible: (custom) => ({
+      scale: 1,
+      opacity: 1,
+      transition: { delay: custom * 0.5 },
+    }),
+  };
+  return (
+    <motion.div
+      viewport={{ once: true }}
+      initial="hidden"
+      whileInView="visible"
+      className="about"
+      id="company">
+      <div className="container">
+        <div className="about__inner">
+          <motion.div variants={AnimateItemLeft} className="about__left">
+            <h3 className="about__left-title">Мы даем гарантию в срок и держим свое слово.</h3>
+            <div className="about__left-text">
+              В мире логистики своевременность и надежность — это ключевые факторы успеха. Мы
+              понимаем, что ваши бизнес-процессы зависят от точности и слаженности поставок, поэтому
+              предлагаем вам услуги, которые соответствуют самым высоким стандартам. Наша компания
+              не просто обещает доставку "в срок" — мы гарантируем это и держим свое слово.
             </div>
-        </motion.div>
-    )
-}
+            <h3 className="about__left-subtitle">B2B целевая аудитория:</h3>
+            <div className="about__left-subtitle-text">
+              Производители, кторым нужнапомощь в экспорте их товаров в страны ближнего востока или
+              в импорте из стран ближнего востока в узбекистан.
+            </div>
+          </motion.div>
+          <motion.div variants={AnimateItemRight} custom={2} className="about__right">
+            <form className="about__form">
+              <h3 className="about__form-title">Связаться с нами</h3>
+              <label className="about__form-input">
+                <p>Имя</p>
+                <input type="text" />
+              </label>
+              <label className="about__form-input">
+                <p>Телефон</p>
+                <input type="text" />
+              </label>
+              <label className="about__form-input">
+                <p>Адрес электронной почты</p>
+                <input type="text" />
+              </label>
+              <button className="about__form-button">Отправить</button>
+            </form>
+          </motion.div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
 
-export default About
+export default About;
--- src/components/HeaderMenu/index.jsx
+++ src/components/HeaderMenu/index.jsx
@@ -1,35 +1,46 @@
-import logo from "../../images/logoHeader.png"
-import "./Header.scss"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
-
+import logo from '../../images/logoHeader.png';
+import './Header.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const HeaderMenu = ({ setBurgerMenu, burgerMenu, dataPartner }) => {
-
   return (
-    <div
-      initial="hidden"
-      whileInView="visible"
-      className="header__menu">
+    <div initial="hidden" whileInView="visible" className="header__menu">
       <div className="container">
-        <div className='header__menu-inner'>
+        <div className="header__menu-inner">
           <a className="header__menu-logo" href="#!">
             <img src={logo} alt="" />
           </a>
-          <div className='header__menu-links'>
-            <ul className={burgerMenu === true ? "header__menu-nav active" : "header__menu-nav"}>
-              <div className='header__menu-close' onClick={() => setBurgerMenu(false)}><FontAwesomeIcon icon={faXmark} /></div>
+          <div className="header__menu-links">
+            <ul className={burgerMenu === true ? 'header__menu-nav active' : 'header__menu-nav'}>
+              <div className="header__menu-close" onClick={() => setBurgerMenu(false)}>
+                <FontAwesomeIcon icon={faXmark} />
+              </div>
 
               {dataPartner.map((item, i) => (
-                <li key={i}><a onClick={() => setBurgerMenu(false)} href={item.link} className='header__menu-link'>{item.text}</a></li>
+                <li key={i}>
+                  <a
+                    onClick={() => setBurgerMenu(false)}
+                    href={item.link}
+                    className="header__menu-link">
+                    {item.text}
+                  </a>
+                </li>
               ))}
               {/* <li><Link to="/#company" className='header__menu-link'>{text}</Link></li>
               <li><Link to="#services" onClick={() => setBurgerMenu(false)} className='header__menu-link'>Наши услуги</Link></li>
               <li><Link to="/partners " onClick={() => setBurgerMenu(false)} className='header__menu-link'>Наши партнеры</Link></li>
               <li><Link to="/#transport" onClick={() => setBurgerMenu(false)} className='header__menu-link'>Транспорт</Link></li> */}
             </ul>
-            <div><a href="tel:+998935059797" className='header__menu-link header__menu-phone'>+998 93 505 97 97</a></div>
+            <a href="tel:+998935059797" className="header__menu-link header__menu-phone">
+              +998 93 505 97 97
+            </a>
+          </div>
+          <div className="header__rejim">
+            <p className="header__rejim-title">Режим работы</p>
+            <p className="header__rejim-time">9:00 - 18:00</p>
+            <p className="header__rejim-hol">Вскресения выходной</p>
           </div>
           <div onClick={() => setBurgerMenu(true)} className="header__menu-burger">
             <span></span>
@@ -37,7 +48,7 @@ const HeaderMenu = ({ setBurgerMenu, burgerMenu, dataPartner }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderMenu
+export default HeaderMenu;
